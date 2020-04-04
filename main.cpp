8bcb60ab99ba72f021d78095f5599e1af15ac60d
prometheus42 <christian@freenono.org>
2020-04-04 08:53:41 +0200

    Set application name in C++ version.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+    app.setOrganizationName("Christian Wichmann");
+    app.setApplicationName("Schnipp!");
+    
     QTranslator translator;
     // look up translations in i18n directory names like e.g. de_DE.qm
     if (translator.load(QLocale().system().name(), QLatin1String("i18n"))) {
